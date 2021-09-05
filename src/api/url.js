2d1c07be2e8f86bbb3f10bf4071f03e09112317d
lexomut <lexomut@mail.ru>
2021-09-05 21:06:02 +0300

    fix: chenge url server

--- src/api/url.js
+++ src/api/url.js
@@ -1,3 +1,3 @@
-const SERVER_URL = 'http://localhost:5000';
-// const SERVER_URL = 'heroku';
+// const SERVER_URL = 'http://localhost:5000';
+const SERVER_URL = 'https://fast-reaches-40751.herokuapp.com/';
 export default SERVER_URL;
