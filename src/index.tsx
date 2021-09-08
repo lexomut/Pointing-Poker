14e0a070ac25870c18e9336ad2d4a8b2262548cc
OKitel <kitelolga@gmail.com>
2021-09-08 22:12:12 +0200

    refactor: reorder imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.module.scss';
 import App from './App';
+import './style.module.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
