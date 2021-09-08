ee04c05d134af706355dbec80372b04588afabf9
lexomut <lexomut@mail.ru>
2021-09-08 17:10:26 +0300

    feat: create read Avatar function

---
+++ src/api/CreateUser.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { User } from '../types/user';
+import SERVER_URL from './url';
+
+export interface CreateUser {
+    firstName: string;
+    lastNme?: string;
+    jobPosition?: string;
+    avatar?: File | undefined;
+}
+export async function createUser({
+    firstName,
+    lastNme,
+    jobPosition,
+    avatar,
+}: CreateUser): Promise<User | string> {
+    const formData = new FormData();
+    formData.append('userConfig', JSON.stringify({ firstName, lastNme, jobPosition }));
+    if (avatar) formData.append('avatar', avatar);
+    try {
+        const response = await axios({
+            method: 'post',
+            url: `${SERVER_URL}/player/`,
+            data: formData,
+            headers: { 'Content-Type': 'multipart/form-data' },
+        });
+        return response.data as User;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+        // TODO :уточнение ошибок
+        return `чтото пошло не так   ${error.response.data}`;
+    }
+}
