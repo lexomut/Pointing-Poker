ad41d88c7093c945e71b14bcb4518aece14f3c82
OKitel <kitelolga@gmail.com>
2021-09-08 21:53:56 +0200

    refactor: change inline styles to class

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
 import { Footer } from './components';
+import styles from './style.module.scss';
 
 const App: React.FC = () => {
     return (
-        <div style={{ position: 'relative', minHeight: '100vh' }}>
+        <div className={styles.container}>
             <Button>pointing-poker</Button>
             <ThreeDRotation />
             <Footer />
