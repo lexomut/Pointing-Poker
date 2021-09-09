f9eda233624743fb7e9e4516afdd5055bb083469
OKitel <kitelolga@gmail.com>
2021-09-09 22:30:20 +0200

    feat: add user card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
-import { Footer } from './components';
+import { UserCard, Footer } from './components';
 import styles from './style.module.scss';
 
 const App: React.FC = () => {
@@ -9,6 +9,27 @@ const App: React.FC = () => {
         <div className={styles.container}>
             <Button>pointing-poker</Button>
             <ThreeDRotation />
+            <UserCard
+                name="Mark"
+                initials="MZ"
+                position="junior front-end developer"
+                onKick={() => {
+                    alert('kick off');
+                }}
+            />
+            <UserCard name="Linda Atkins" initials="LA" position="senior" currentUser />
+            <UserCard
+                name="Barsik"
+                initials="JL"
+                position="junior lion"
+                imgSrc="https://placekitten.com/200/200"
+            />
+            <UserCard
+                name="Max "
+                initials="MM"
+                position="middle"
+                imgSrc="https://image.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg"
+            />
             <Footer />
         </div>
     );
---
+++ src/components/UserCard/UserCard.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import clsx from 'clsx';
+import { Avatar } from '@material-ui/core';
+import BlockIcon from '@material-ui/icons/Block';
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 250,
+        width: 250,
+        height: 60,
+        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        marginBottom: 10,
+    },
+    cardContainer: {
+        position: 'relative',
+        padding: 0,
+        paddingLeft: 5,
+        display: 'flex',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    button: {
+        position: 'absolute',
+        bottom: 20,
+        right: 0,
+    },
+    title: {
+        lineHeight: 1,
+    },
+    caption: {
+        fontSize: '0.6rem',
+    },
+    avatar: {
+        background: '#60DABF',
+        boxShadow: 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)',
+        textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        fontWeight: 'bold',
+        marginRight: 10,
+    },
+    currentUser: {
+        background: 'rgba(96, 218, 191, 0.33)',
+    },
+});
+
+type Props = {
+    currentUser?: boolean;
+    name: string;
+    position: string;
+    initials: string;
+    imgSrc?: string;
+    onKick?: () => void;
+};
+
+export const UserCard: React.FC<Props> = (props: Props) => {
+    const { name, currentUser, position, onKick, initials, imgSrc } = props;
+    const classes = useStyles();
+
+    return (
+        <Card
+            className={clsx(classes.root, { [classes.currentUser]: currentUser })}
+            variant="outlined"
+        >
+            <CardContent className={classes.cardContainer}>
+                {imgSrc && <Avatar className={classes.avatar} src={imgSrc} alt={name} />}
+                {!imgSrc && <Avatar className={classes.avatar}>{initials}</Avatar>}
+                <div>
+                    <Typography variant="h6" className={classes.title}>
+                        {name}
+                    </Typography>
+                    <Typography color="textSecondary" variant="caption" className={classes.caption}>
+                        {position}
+                    </Typography>
+                </div>
+                {!currentUser && (
+                    <CardActions className={classes.button}>
+                        <Button onClick={onKick}>
+                            <BlockIcon />
+                        </Button>
+                    </CardActions>
+                )}
+            </CardContent>
+        </Card>
+    );
+};
+
+UserCard.defaultProps = {
+    currentUser: false,
+    imgSrc: undefined,
+    onKick: () => {},
+};
