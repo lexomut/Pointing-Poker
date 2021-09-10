65bc63157d40a1f66d93fab22213048daa85a9cd
OKitel <kitelolga@gmail.com>
2021-09-10 23:01:42 +0200

    refactor: get rid of position absolute

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,11 @@ import styles from './style.module.scss';
 const App: React.FC = () => {
     return (
         <div className={styles.container}>
-            <Button>pointing-poker</Button>
-            <ThreeDRotation />
+            <div className={styles.fakeHeader}>Header will be here</div>
+            <div className={styles.fakeContent}>
+                <Button>pointing-poker</Button>
+                <ThreeDRotation />
+            </div>
             <Footer />
         </div>
     );
