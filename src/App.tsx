3a594bf7e65202c5cc2624cfe55dedf5d4734b01
OKitel <kitelolga@gmail.com>
2021-09-11 02:09:46 +0200

    feat: add router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
 import { Footer } from './components';
@@ -7,8 +8,25 @@ import styles from './style.module.scss';
 const App: React.FC = () => {
     return (
         <div className={styles.container}>
-            <Button>pointing-poker</Button>
-            <ThreeDRotation />
+            <Switch>
+                <Route exact path="/">
+                    Main
+                    <Button>pointing-poker</Button>
+                    <ThreeDRotation />
+                </Route>
+                <Route exact path="/lobby">
+                    Lobby
+                </Route>
+                <Route exact path="/settings">
+                    Settings for dealer
+                </Route>
+                <Route exact path="/game">
+                    Game
+                </Route>
+                <Route exact path="*">
+                    404
+                </Route>
+            </Switch>
             <Footer />
         </div>
     );
