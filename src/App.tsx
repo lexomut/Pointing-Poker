be27c857e390642c0d2411d911ff4e949f0415b4
OKitel <kitelolga@gmail.com>
2021-09-14 20:26:39 +0200

    refactor: change div to diamond, transfer styles to index.scss for root

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ import styles from './style.module.scss';
 
 const App: React.FC = () => {
     return (
-        <div className={styles.container}>
+        <>
             <div className={styles.fakeHeader}>Header will be here</div>
             <div className={styles.fakeContent}>
                 <Button>pointing-poker</Button>
                 <ThreeDRotation />
             </div>
             <Footer />
-        </div>
+        </>
     );
 };
 
