98102d9e59ad079af73ec79e525d74e7bafaea59
OKitel <kitelolga@gmail.com>
2021-09-15 14:41:23 +0200

    feat: make private routes

--- src/components/invitation/Invitation.tsx
+++ src/components/invitation/Invitation.tsx
@@ -3,7 +3,10 @@ import { Route, Switch, useRouteMatch } from 'react-router-dom';
 
 export const Invitation: React.FC = () => {
     const { path } = useRouteMatch();
-    return (
+    const isAuthorized = true; // DEMO => TODO replace with selector from REDUX
+    const isDealer = true;
+
+    return isAuthorized ? (
         <Switch>
             <Route exact path={`${path}`}>
                 Popup to connect
@@ -11,11 +14,22 @@ export const Invitation: React.FC = () => {
             <Route exact path={`${path}/lobby`}>
                 Lobby for all players
             </Route>
-            <Route exact path={`${path}/settings`}>
-                Lobby for dealer
-            </Route>
+            {isDealer && (
+                <Route exact path={`${path}/settings`}>
+                    Lobby for dealer
+                </Route>
+            )}
             <Route exact path={`${path}/game`}>
                 Game
+            </Route>
+            <Route exact path="*">
+                404
+            </Route>
+        </Switch>
+    ) : (
+        <Switch>
+            <Route exact path={`${path}`}>
+                Popup to connect
             </Route>
             <Route exact path="*">
                 404
