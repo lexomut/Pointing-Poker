4eedfbd0ba780d99b9a7b833eef56e98f23dbe22
OKitel <kitelolga@gmail.com>
2021-09-15 15:17:27 +0200

    feat: add chat icon button to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { AppBar, Fab, makeStyles, Toolbar } from '@material-ui/core';
+import React, { useState } from 'react';
+import { AppBar, Fab, IconButton, makeStyles, Toolbar } from '@material-ui/core';
+import SpeakerNotesIcon from '@material-ui/icons/SpeakerNotes';
+import SpeakerNotesOffIcon from '@material-ui/icons/SpeakerNotesOff';
 import pokerIcon from './assets/transparent_dark_icon.png';
 
 const useStyles = makeStyles({
@@ -9,20 +11,44 @@ const useStyles = makeStyles({
     },
     fab: {
         position: 'relative',
-        left: '50%',
+        right: '50%',
         top: 30,
         background: '#a7ffeb',
+    },
+    header: {
+        display: 'flex',
+        justifyContent: 'flex-end',
     },
 });
 
 export const Header: React.FC = () => {
     const classes = useStyles();
+    const [isChatOpen, setIsChatOpen] = useState(false);
     return (
         <AppBar color="primary" position="static">
-            <Toolbar>
+            <Toolbar className={classes.header}>
                 <Fab className={classes.fab}>
                     <img className={classes.img} src={pokerIcon} alt="pointing poker" />
                 </Fab>
+                {!isChatOpen ? (
+                    <IconButton
+                        color="secondary"
+                        onClick={() => {
+                            setIsChatOpen(true);
+                        }}
+                    >
+                        <SpeakerNotesIcon />
+                    </IconButton>
+                ) : (
+                    <IconButton
+                        color="secondary"
+                        onClick={() => {
+                            setIsChatOpen(false);
+                        }}
+                    >
+                        <SpeakerNotesOffIcon />
+                    </IconButton>
+                )}
             </Toolbar>
         </AppBar>
     );
