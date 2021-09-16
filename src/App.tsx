6cb6e2c7ad116c0970f9346ffc09aae4b8b32ade
OKitel <kitelolga@gmail.com>
2021-09-16 17:02:29 +0200

    fix: fix spaces after merging

--- src/App.tsx
+++ src/App.tsx
@@ -35,33 +35,33 @@ const App: React.FC = () => {
                 <Header />
                 <main className={styles.content}>
                     <Switch>
-                    <Route exact path="/">
-                        Main
-                        <Button
-                        color="primary"
-                        variant="contained"
-                        onClick={() => alert('Put logic here')}
-
-                        >start game
-                    </Button>
-                    <Button
-                        color="primary"
-                        variant="outlined"
-                        onClick={() => alert('Put logic here')}
-                        >
-                        cancel game
-                    </Button>
-                    <IssueButton />
-                    <DemoGameCards />
-                    <DemoUserCards />
-                    <IssueCard name="Issue 234" priority="High" dealer />
-                    <IssueCard current name="Issue 235" priority="Low" />
-                    <Switcher label="Scrum master as player:" />
-                    </Route>
-                    <Route path="/:id">
-                        <Invitation />
-                    </Route>
-                </Switch>
+                        <Route exact path="/">
+                            Main
+                            <Button
+                                color="primary"
+                                variant="contained"
+                                onClick={() => alert('Put logic here')}
+                            >
+                                start game
+                            </Button>
+                            <Button
+                                color="primary"
+                                variant="outlined"
+                                onClick={() => alert('Put logic here')}
+                            >
+                                cancel game
+                            </Button>
+                            <IssueButton />
+                            <DemoGameCards />
+                            <DemoUserCards />
+                            <IssueCard name="Issue 234" priority="High" dealer />
+                            <IssueCard current name="Issue 235" priority="Low" />
+                            <Switcher label="Scrum master as player:" />
+                        </Route>
+                        <Route path="/:id">
+                            <Invitation />
+                        </Route>
+                    </Switch>
                 </main>
                 <Footer />
             </MuiThemeProvider>
