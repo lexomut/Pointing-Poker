1e52bff5fe56e9e9b52cb7afd3aa5fee9fc2681e
OKitel <kitelolga@gmail.com>
2021-09-16 16:33:34 +0200

    fix: move demo content into main route

--- src/App.tsx
+++ src/App.tsx
@@ -21,23 +21,31 @@ const App: React.FC = () => {
                 <Switch>
                     <Route exact path="/">
                         Main
+                        <Button
+                            color="primary"
+                            variant="contained"
+                            onClick={() => alert('Put logic here')}
+                        >
+                            start game
+                        </Button>
+                        <Button
+                            color="primary"
+                            variant="outlined"
+                            onClick={() => alert('Put logic here')}
+                        >
+                            cancel game
+                        </Button>
+                        <IssueButton />
+                        <DemoGameCards />
+                        <DemoUserCards />
+                        <IssueCard name="Issue 234" priority="High" dealer />
+                        <IssueCard current name="Issue 235" priority="Low" />
+                        <Switcher label="Scrum master as player:" />
                     </Route>
                     <Route path="/:id">
                         <Invitation />
                     </Route>
                 </Switch>
-                <Button color="primary" variant="contained" onClick={() => alert('Put logic here')}>
-                    start game
-                </Button>
-                <Button color="primary" variant="outlined" onClick={() => alert('Put logic here')}>
-                    cancel game
-                </Button>
-                <IssueButton />
-                <DemoGameCards />
-                <DemoUserCards />
-                <IssueCard name="Issue 234" priority="High" dealer />
-                <IssueCard current name="Issue 235" priority="Low" />
-                <Switcher label="Scrum master as player:" />
             </main>
             <Footer />
         </>
