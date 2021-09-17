edc9792868d949faa779f8782ac5fff99e0e838b
OKitel <kitelolga@gmail.com>
2021-09-17 21:20:03 +0200

    fix: fix spaces after merge

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import { AppBar, createStyles, Fab, IconButton, makeStyles, Theme, Toolbar } from '@material-ui/core';
+import {
+    AppBar,
+    createStyles,
+    Fab,
+    IconButton,
+    makeStyles,
+    Theme,
+    Toolbar,
+} from '@material-ui/core';
 import SpeakerNotesIcon from '@material-ui/icons/SpeakerNotes';
 import SpeakerNotesOffIcon from '@material-ui/icons/SpeakerNotesOff';
 import pokerIcon from './assets/transparent_dark_icon.png';
@@ -18,8 +26,10 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         header: {
             marginBottom: 30,
+        },
+        header_bar: {
             display: 'flex',
-        justifyContent: 'flex-end',
+            justifyContent: 'flex-end',
         },
     }),
 );
@@ -29,7 +39,7 @@ export const Header: React.FC = () => {
     const [isChatOpen, setIsChatOpen] = useState(false);
     return (
         <AppBar className={classes.header} color="primary" position="static">
-            <Toolbar className={classes.header}>
+            <Toolbar className={classes.header_bar}>
                 <Fab className={classes.fab}>
                     <img className={classes.img} src={pokerIcon} alt="pointing poker" />
                 </Fab>
