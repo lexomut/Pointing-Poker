3a23ef55fd15478a01e7476d3b4c90faa067b9d2
LenarFF <flfforwork@gmail.com>
2021-09-18 23:07:11 +0300

    fix: delete cards

--- src/Components/GameCard/GameCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ICard } from '../../types';
-import styles from './GameCard.module.css';
-
-const GameCard: React.FC<ICard> = ({ rating }) => {
-    return <div className={styles.playCard}>{rating}</div>;
-};
-
-export default GameCard;
--- src/Components/IssueCard/IssueCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { IIssues } from '../../types';
-import styles from './IssueCard.module.css';
-
-const IssueCard: React.FC<IIssues> = ({ issueNumber, priority }) => {
-    return <div className={styles.issueCard}>{`${issueNumber} ${priority}`}</div>;
-};
-
-export default IssueCard;
