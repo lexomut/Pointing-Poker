03706dcdd01c93e484b159747e757c45c91b9e97
OKitel <kitelolga@gmail.com>
2021-09-29 20:24:55 +0200

    fix: change styles for link

--- src/Components/LinkToLobby/LinkToLobby.tsx
+++ src/Components/LinkToLobby/LinkToLobby.tsx
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
-import Button from '@material-ui/core/Button/Button';
+import { Button, Grid, TextField } from '@material-ui/core';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 import { GlobalContext } from '../../state/Context';
 import { GlobalState } from '../../types/GlobalState';
-
 import styles from './LinkToLobby.module.scss';
 
 export const LinkToLobby: () => JSX.Element = () => {
@@ -11,19 +11,31 @@ export const LinkToLobby: () => JSX.Element = () => {
 
     return (
         <div className={styles.linkToLobby}>
-            <p>Link to Lobby:</p>
-            <div className={styles.linkToLobby__controls}>
-                <div className={styles.linkToLobby__controls__url}>{currentURL}</div>
-                <Button
-                    color="primary"
-                    variant="contained"
-                    onClick={() => {
-                        navigator.clipboard.writeText(currentURL);
-                    }}
-                >
-                    Copy
-                </Button>
-            </div>
+            <Grid container>
+                <Grid item xs={12}>
+                    <TextField
+                        id="outlined-read-only-input"
+                        label="Link to lobby"
+                        fullWidth
+                        defaultValue={currentURL}
+                        InputProps={{
+                            readOnly: true,
+                            endAdornment: (
+                                <Button
+                                    color="primary"
+                                    variant="contained"
+                                    onClick={() => {
+                                        navigator.clipboard.writeText(currentURL);
+                                    }}
+                                    endIcon={<FileCopyIcon />}
+                                >
+                                    Copy
+                                </Button>
+                            ),
+                        }}
+                    />
+                </Grid>
+            </Grid>
         </div>
     );
 };
