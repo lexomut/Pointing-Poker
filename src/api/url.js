794a2b8f6aab3feca660e91c162f77e84d02f0d1
lexomut <lexomut@mail.ru>
2021-10-04 19:14:51 +0300

    fix:checkVoted

--- src/api/url.js
+++ src/api/url.js
@@ -1,3 +1,3 @@
-// export const SERVER_URL = 'http://localhost:5000/';
+export const SERVER_URL = 'http://localhost:5000/';
 // export const SERVER_URL = 'https://fast-reaches-40751.herokuapp.com/';
-export const SERVER_URL = 'http://35.217.41.219:5000/';
+// export const SERVER_URL = 'http://35.217.41.219:5000/';
