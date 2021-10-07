af2d34bd73891fd56ebc3095063a2e96838eb4ec
OKitel <kitelolga@gmail.com>
2021-10-08 01:28:33 +0200

    feat: add first test: app rendering

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
 test('renders learn react link', () => {
-    render(<App />);
+    render(
+        <Router>
+            <App />
+        </Router>,
+    );
 
-    const linkElement = screen.getByText(/scrum/i);
+    const linkElement = screen.getByText(/planning/i);
 
     expect(linkElement).toBeInTheDocument();
 });
