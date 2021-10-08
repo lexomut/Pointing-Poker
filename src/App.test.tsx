899fb39660b260af74047d975b01e9be5cd5006c
OKitel <kitelolga@gmail.com>
2021-10-08 14:09:17 +0200

    feat: add simple tests for all pages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,14 +3,24 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
-test('renders learn react link', () => {
+test('renders learn react main page', () => {
     render(
         <Router>
             <App />
         </Router>,
     );
 
-    const linkElement = screen.getByText(/planning/i);
+    const pageElement = screen.getByText(/planning/i);
 
-    expect(linkElement).toBeInTheDocument();
+    expect(pageElement).toBeInTheDocument();
+});
+
+test('renders app', () => {
+    const app = render(
+        <Router>
+            <App />
+        </Router>,
+    );
+
+    expect(app).not.toBeNull();
 });
